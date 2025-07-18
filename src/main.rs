39218d0eeddf9e2b3e7260b99fbaba6b18c715d5
ngrigorian <ngrigorian@dyndrite.com>
2025-07-18 16:25:46 -0400

    We now try to strip alpha channels

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::{env::set_current_dir, fs, os::windows::fs::MetadataExt, path::PathBuf, thread};
 
 use clap::Parser;
-use image::{codecs::png::PngEncoder, imageops::{resize, FilterType::Gaussian}, ImageReader};
+use image::{codecs::png::PngEncoder, imageops::{resize, FilterType::Gaussian}, DynamicImage, GenericImage, GenericImageView, ImageReader, Rgb, RgbImage};
 use tempfile::NamedTempFile;
 
 /// A helper util that will search for pngs in the current directory tree and then compress them
@@ -20,39 +20,64 @@ struct Args {
     dir: Option<String>,
 }
 
-fn compress_images(infile_name: &String, outfile_name: &String, max_width: Option<u32>, max_height: Option<u32>) -> Result<(), Box<dyn std::error::Error>> {
-    let temp_path = NamedTempFile::new()?;
-    let loaded_image = ImageReader::open(infile_name)?.decode()?;
-
-    let (nwidth, nheight) = match (max_width, max_height) {
-        (None, None) => (loaded_image.width(), loaded_image.height()),
-        (None, Some(max_h)) => ((loaded_image.width() as f32 * (max_h as f32 / loaded_image.height() as f32)) as u32, max_h),
-        (Some(max_w), None) => (max_w, ((loaded_image.height() as f32 * (max_w as f32 / loaded_image.width() as f32)) as u32)),
-        (Some(max_w), Some(max_h)) => {
-            let w_ratio = (max_w as f32 / loaded_image.width() as f32).min(1.0);
-            let h_ratio = (max_h as f32 / loaded_image.height() as f32).min(1.0);
-            ((loaded_image.width() as f32 * w_ratio.min(h_ratio)) as u32,
-            (loaded_image.height() as f32 * w_ratio.min(h_ratio)) as u32)
-        },
-    };
-
-    let smaller_image = resize(&loaded_image, nwidth, nheight, Gaussian);
-    let png_encoder = PngEncoder::new_with_quality(&temp_path, image::codecs::png::CompressionType::Best, image::codecs::png::FilterType::Adaptive);
-    smaller_image.write_with_encoder(png_encoder)?;
-
-    if let Ok(true) = fs::exists(outfile_name) {
-        let target_metadata = fs::metadata(outfile_name)?;
-        let temp_metadata = fs::metadata(temp_path.path())?;
-        if target_metadata.file_size() < temp_metadata.file_size() {
-            return Ok(());
-        }
-        let mut perms = std::fs::metadata(outfile_name)?.permissions();
-        if perms.readonly() {
-            perms.set_readonly(false);
-            std::fs::set_permissions(outfile_name, perms)?;
-        }
+fn load_and_preprocess(file_path: &String) -> Result<Vec<DynamicImage>, Box<dyn std::error::Error>> {
+    let loaded_image = ImageReader::open(file_path)?.decode()?;
+    if !loaded_image.color().has_alpha() {
+        return Ok(vec![loaded_image]);
     }
-    Ok(std::fs::rename(temp_path.path(), outfile_name)?)
+    
+    if loaded_image.pixels().into_iter().any(|p| p.2.0[3] < 254) {
+        return Ok(vec![loaded_image]);
+    } else {
+        let mut stripped_image = RgbImage::new(loaded_image.width(), loaded_image.height());
+        for pixel in loaded_image.pixels() {
+            stripped_image.put_pixel(pixel.0, pixel.1, Rgb([pixel.2.0[0], pixel.2.0[1], pixel.2.0[2]]));
+        }
+        Ok(vec![loaded_image.clone(), stripped_image.into()])
+    }
+}
+
+fn compress_image(loaded_image: DynamicImage, outfile_name: &String, nwidth: u32, nheight: u32) -> Result<(), Box<dyn std::error::Error>> {
+
+        let temp_path = NamedTempFile::new()?;
+        let smaller_image = resize(&loaded_image, nwidth, nheight, Gaussian);
+        let png_encoder = PngEncoder::new_with_quality(&temp_path, image::codecs::png::CompressionType::Best, image::codecs::png::FilterType::Adaptive);
+        smaller_image.write_with_encoder(png_encoder)?;
+
+        if let Ok(true) = fs::exists(outfile_name) {
+            let target_metadata = fs::metadata(outfile_name)?;
+            let temp_metadata = fs::metadata(temp_path.path())?;
+            if target_metadata.file_size() < temp_metadata.file_size() {
+                return Ok(());
+            }
+            let mut perms = std::fs::metadata(outfile_name)?.permissions();
+            if perms.readonly() {
+                perms.set_readonly(false);
+                std::fs::set_permissions(outfile_name, perms)?;
+            }
+        }
+        Ok(std::fs::rename(temp_path.path(), outfile_name)?)
+}
+
+fn compress_images(infile_name: &String, outfile_name: &String, max_width: Option<u32>, max_height: Option<u32>) -> Result<(), Box<dyn std::error::Error>> {
+    let loaded_images = load_and_preprocess(infile_name)?;
+    for loaded_image in loaded_images {
+        let (nwidth, nheight) = match (max_width, max_height) {
+            (None, None) => (loaded_image.width(), loaded_image.height()),
+            (None, Some(max_h)) => ((loaded_image.width() as f32 * (max_h as f32 / loaded_image.height() as f32)) as u32, max_h),
+            (Some(max_w), None) => (max_w, ((loaded_image.height() as f32 * (max_w as f32 / loaded_image.width() as f32)) as u32)),
+            (Some(max_w), Some(max_h)) => {
+                let w_ratio = (max_w as f32 / loaded_image.width() as f32).min(1.0);
+                let h_ratio = (max_h as f32 / loaded_image.height() as f32).min(1.0);
+                ((loaded_image.width() as f32 * w_ratio.min(h_ratio)) as u32,
+                (loaded_image.height() as f32 * w_ratio.min(h_ratio)) as u32)
+            },
+        };
+
+        compress_image(loaded_image, outfile_name, nwidth, nheight)?;
+    }
+
+    Ok(())
 }
 
 fn find_png_paths(path: &String) -> Vec<String>  {
