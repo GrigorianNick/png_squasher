88014adf848c46a60780590f3fdce194e9717923
ngrigorian <ngrigorian@dyndrite.com>
2025-07-18 02:40:45 -0400

    Added in resizing

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,44 @@
-use std::{fs::{self}, io::{BufWriter, Error}, os::windows::fs::MetadataExt, path::PathBuf, thread};
+use std::{env::set_current_dir, fs, os::windows::fs::MetadataExt, path::PathBuf, thread};
 
 use clap::Parser;
-use png::EncodingError;
+use image::{codecs::png::PngEncoder, imageops::{resize, FilterType::Gaussian}, ImageReader};
 use tempfile::NamedTempFile;
 
-extern crate png;
-
+/// A helper util that will search for pngs in the current directory tree and then compress them
 #[derive(Parser, Debug)]
 struct Args {
+    /// Maximum number of pixels pngs are allowed to have on the x axis. Larger images will be scaled down
+    #[arg(short, long)]
+    x_max: Option<u32>,
+
+    /// Maximum number of pixels pngs are allowed to have on the y axis. Larger images will be scaled down
+    #[arg(short, long)]
+    y_max: Option<u32>,
+
+    /// Directory to start the recursive png search
+    #[arg(short, long)]
+    dir: Option<String>,
 }
 
-fn compress_file(infile_name: &String, outfile_name: &String) -> Result<(), Error> {
-    println!("Compressing:{}...", infile_name);
-    let decoder =    png::Decoder::new(std::fs::File::open(infile_name)?);
-
-    let mut reader = decoder.read_info()?;
-    let header_info = reader.info().clone();
-    let mut buf = vec![0; reader.output_buffer_size()];
-    let info = reader.next_frame(&mut buf)?;
-    let bytes = &buf[..info.buffer_size()];
-
+fn compress_images(infile_name: &String, outfile_name: &String, max_width: Option<u32>, max_height: Option<u32>) -> Result<(), Box<dyn std::error::Error>> {
     let temp_path = NamedTempFile::new()?;
-    let ref mut w = BufWriter::new(temp_path.as_file());
-    
-    let mut encoder = png::Encoder::new(w, info.width, info.height);
-    encoder.set_color(info.color_type);
-    encoder.set_depth(info.bit_depth);
-    if let Some(palette) = header_info.palette {
-        encoder.set_palette(palette);
-    }
-    encoder.set_compression(png::Compression::Best);
-    encoder.set_adaptive_filter(png::AdaptiveFilterType::Adaptive);
-    encoder.set_filter(png::FilterType::Paeth);
-    let mut writer = encoder.write_header()?;
+    let loaded_image = ImageReader::open(infile_name)?.decode()?;
 
-    writer.write_image_data(&bytes)?;
-    writer.finish()?;
+    let (nwidth, nheight) = match (max_width, max_height) {
+        (None, None) => (loaded_image.width(), loaded_image.height()),
+        (None, Some(max_h)) => ((loaded_image.width() as f32 * (max_h as f32 / loaded_image.height() as f32)) as u32, max_h),
+        (Some(max_w), None) => (max_w, ((loaded_image.height() as f32 * (max_w as f32 / loaded_image.width() as f32)) as u32)),
+        (Some(max_w), Some(max_h)) => {
+            let w_ratio = (max_w as f32 / loaded_image.width() as f32).min(1.0);
+            let h_ratio = (max_h as f32 / loaded_image.height() as f32).min(1.0);
+            ((loaded_image.width() as f32 * w_ratio.min(h_ratio)) as u32,
+            (loaded_image.height() as f32 * w_ratio.min(h_ratio)) as u32)
+        },
+    };
+
+    let smaller_image = resize(&loaded_image, nwidth, nheight, Gaussian);
+    let png_encoder = PngEncoder::new_with_quality(&temp_path, image::codecs::png::CompressionType::Best, image::codecs::png::FilterType::Adaptive);
+    smaller_image.write_with_encoder(png_encoder)?;
 
     if let Ok(true) = fs::exists(outfile_name) {
         let target_metadata = fs::metadata(outfile_name)?;
@@ -49,7 +52,6 @@ fn compress_file(infile_name: &String, outfile_name: &String) -> Result<(), Erro
             std::fs::set_permissions(outfile_name, perms)?;
         }
     }
-
     Ok(std::fs::rename(temp_path.path(), outfile_name)?)
 }
 
@@ -82,13 +84,20 @@ fn find_png_paths(path: &String) -> Vec<String>  {
     png_entries.into_iter().chain(child_pngs).collect()
 }
 
-fn main() -> Result<(), EncodingError> {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+
+    let args = Args::parse();
+
+    if let Some(path) = args.dir {
+        set_current_dir(path)?;
+    }
+
     let cwd = String::from(".");
     let pngs = find_png_paths(&cwd);
     let mut handles = vec![];
     for png in pngs {
         handles.push(thread::spawn(move || {
-            if let Err(e) =  compress_file(&png, &png) {
+            if let Err(e) =  compress_images(&png, &png, args.x_max, args.y_max) {
                 println!("{}:{}", png, e);
             }
         }));
